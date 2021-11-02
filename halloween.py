54578ee375a7fce138cf44a82053de4a28315d9e
rjmclachlan <64862861+rjmclachlan@users.noreply.github.com>
2021-11-02 12:11:09 -0700

    11/2 update

--- halloween.py
+++ halloween.py
@@ -10,6 +10,10 @@ df = pd.DataFrame({'Date': ["103121 17:30", " 103121 18:30", " 103121 19:00", "1
                    'Kids': [0, 100, 210, 300, 400, 450, 500, 526]})
 df['Date'] = pd.to_datetime(df['Date'])
 
+# Here is a slope line
+x = (df.Date[0], df.Date[7])
+y = (df.Kids[0], df.Kids[7])
+
 # Configure plot background
 plt.rcParams['figure.dpi'] = 150
 plt.style.use('dark_background')
@@ -17,24 +21,19 @@ plt.style.use('dark_background')
 # create plot
 fig = plt.figure()
 ax = plt.axes()
-
-# Here is a slope line
-x = (df.Date[0], df.Date[7])
-y = (df.Kids[0], df.Kids[7])
+ax.plot(x, y, linestyle='--', label='Slope (2.5 Kids per Min)', color='green')
+ax.plot(df.Date, df.Kids, linewidth=3, label='Realtime (526 Kids Total)', color='red')
 
 # Graph Presentation
+
 ax.xaxis.grid(color='white', linestyle='dashed', which='both', alpha=0.5)
 ax.yaxis.grid(color='white', linestyle='solid', which='both', alpha=0.1)
 ax.set_title('Halloween Trick or Treaters over Time\n', loc='center', fontsize=16, color='orange')
 ax.set_ylabel('Number of Trick of Treaters', fontsize=12, color='orange')
 ax.set_xlabel('Halloween Evening', fontsize=12, color='orange')
-#handels = ['slope', 'Realtime']
-#labels = ['slope', 'Realtime']
-#ax.legend(handels, labels, loc='lower right')
 myFmt = DateFormatter("%H:%M")
 ax.xaxis.set_major_formatter(myFmt)
+ax.legend(loc='lower right')
 
-# Run and display graph
-ax.plot(x, y, linestyle='--', label='slope', color='green')
-ax.plot(df.Date, df.Kids, linewidth=3, label='Realtime', color='red')
+# Display graph
 plt.show()
